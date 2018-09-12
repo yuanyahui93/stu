982a14357015f81e92b374ef9aea0e6f3f2f4db0
yuanyahui93 <yuanyahui93@163.com>
2018-09-12 14:22:40 +0800

    jttt

---
+++ jt-eureka-server/src/main/java/com/jt/RunAppServer.java
@@ -0,0 +1,14 @@
+package com.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RunAppServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunAppServer.class, args);
+	}
+}
